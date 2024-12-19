a4c4341c78d1bbbf64cf7d7e27635061c6e6161b
GokuSan <81345800+GokuSan2@users.noreply.github.com>
2024-12-19 14:24:45 +0530

    Add files via upload

---
+++ clone.sh
@@ -0,0 +1,6 @@
+git clone -b lineage-22.0 https://github.com/xiaomi-sm6150/android_device_xiaomi_sweet.git device/xiaomi/sweet
+git clone -b lineage-22.0 https://github.com/xiaomi-sm6150/android_device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+git clone -b lineage-22.0 https://github.com/xiaomi-sm6150/proprietary_vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+git clone -b lineage-22.0 https://github.com/xiaomi-sm6150/proprietary_vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+git clone -b sleepy --depth=1 https://github.com/GokuSan2/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone -b 15 https://github.com/GokuSan2/android_hardware_xiaomi.git hardware/xiaomi
